8e14e2ab5cba17a6121b3e0c517f0a975ecb28c2
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-06 21:26:56 +0900

    Use got instead of ky-universal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GetServerSideProps } from 'next'
-import ky from 'ky-universal'
+import got from 'got'
 
 const HomePage = ({ user }: { user: any }) => {
   const oauthUrl = `https://github.com/login/oauth/authorize?client_id=28d9036cc07644ae74c3`
@@ -15,7 +15,7 @@ const HomePage = ({ user }: { user: any }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const user = await ky('http://localhost:3000/api', {
+  const user = await got('http://localhost:3000/api', {
     headers: {
       cookie: context.req.headers.cookie
     }
