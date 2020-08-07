97c05a080a60c42d689228617f80659d90e3390a
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 18:59:16 +0900

    Create an API that creates a dayLog

---
+++ pages/api/dayLogs/index.ts
@@ -0,0 +1,28 @@
+import { redirect } from 'prismy'
+import { methodRouter } from 'prismy-method-router'
+import { p } from '../../../lib/p'
+import { bodySelector, authenticatedUserSelector } from '../../../lib/selectors'
+import { createError } from 'prismy'
+import { DayLog } from '../../../lib/models'
+
+export const createDayLog = p(
+  [authenticatedUserSelector, bodySelector],
+  async (user, body) => {
+    if (user == null) {
+      throw createError(401, 'Unauthorized user')
+    }
+    const { date, title, content } = body
+    const dayLog = await DayLog.create({
+      date,
+      title,
+      content,
+      userId: user.id
+    })
+
+    return redirect('/')
+  }
+)
+
+export default methodRouter({
+  post: createDayLog
+})
