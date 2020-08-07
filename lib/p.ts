083a7cec52c29074a21c6e915cfac4cf6788c758
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 19:27:30 +0900

    Give default value to middlewares

--- lib/p.ts
+++ lib/p.ts
@@ -7,5 +7,5 @@ const defaultMiddlewares = [sessionMiddleware, withErrorHandler]
 export const p: typeof prismy = (
   selectors: any,
   handler: any,
-  middlewares?: any
+  middlewares: any = []
 ) => prismyx(selectors, handler, [...middlewares, ...defaultMiddlewares])
