2328cfe1b24d8beb82e8f2cc2e8a21e22a712ddb
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 18:47:21 +0900

    Use .env variables

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize('tid', 'root', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  '',
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+)
 
 export default sequelize
