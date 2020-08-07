538d9b7e8129c7ce8654226945b50de51f45a548
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 19:35:40 +0900

    Pass default value for env variables

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
+  process.env.DB_NAME || 'tid',
+  process.env.DB_USERNAME || 'root',
   '',
   {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: false
   }
 )
 
